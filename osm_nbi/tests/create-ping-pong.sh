d125caf630021538f19aadab045b5d17def67cdd
tierno <alfonso.tiernosepulveda@telefonica.com>
2018-11-22 16:05:54 +0000

    (bug 554) adding license headers
    Change-Id: Ife52d39b8bc8dcce5cbc26665ef15271d738d2a7
    Signed-off-by: tierno <alfonso.tiernosepulveda@telefonica.com>

--- osm_nbi/tests/create-ping-pong.sh
+++ osm_nbi/tests/create-ping-pong.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 export NBI_URL=https://localhost:9999/osm
 USERNAME=admin
 PASSWORD=admin
@@ -30,7 +43,6 @@ TOKEN=`curl --insecure -H "Content-Type: application/yaml" -H "Accept: applicati
      2>/dev/null | awk '($1=="id:"){print $2}'`;
 echo token: $TOKEN
 
-
 # VNFD
 #########
 #insert PKG
@@ -44,8 +56,6 @@ VNFD2_ID=`curl --insecure -w "%{http_code}\n" -H "Content-Type: application/gzip
      2>/dev/null | awk '($1=="id:"){print $2}'`
 echo pong_vnfd: $VNFD2_ID
 
-
-
 # NSD
 #########
 #insert PKG
@@ -53,7 +63,6 @@ NSD1_ID=`curl --insecure -w "%{http_code}\n" -H "Content-Type: application/gzip"
     -H "Authorization: Bearer $TOKEN"   --data-binary "@$NSD1" ${NBI_URL}/nsd/v1/ns_descriptors_content \
     2>/dev/null | awk '($1=="id:"){print $2}'`
 echo ping_pong_nsd: $NSD1_ID
-
 
 # NSRS
 ##############
